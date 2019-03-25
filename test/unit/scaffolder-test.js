b71adeb2cd7117bfbaf1446102e99724b9b4b3a1
Matt Travi <programmer@travi.org>
2019-03-24 23:45:50 -0500

    feat(ci): enabled scaffolding of the chosen service
    closes #4

--- test/unit/scaffolder-test.js
+++ test/unit/scaffolder-test.js
@@ -4,6 +4,7 @@ import fs from 'mz/fs';
 import {assert} from 'chai';
 import any from '@travi/any';
 import sinon from 'sinon';
+import * as ciScaffolder from '../../src/ci-scaffolder';
 import {scaffold} from '../../src';
 
 suite('scaffolder', () => {
@@ -22,6 +23,7 @@ suite('scaffolder', () => {
     sandbox.stub(fs, 'writeFile');
     sandbox.stub(inquirer, 'prompt');
     sandbox.stub(commonLanguagePrompts, 'questions');
+    sandbox.stub(ciScaffolder, 'default');
 
     commonLanguagePrompts.questions.withArgs({vcs, ciServices, visibility}).returns(questions);
   });
@@ -29,8 +31,19 @@ suite('scaffolder', () => {
   teardown(() => sandbox.restore());
 
   test('that the package file and Makefile are written', async () => {
+    const chosenService = any.word();
+    const ciFilesToIgnore = any.listOf(any.word);
+    const ciDirectoriesToIgnore = any.listOf(any.word);
     fs.writeFile.resolves();
-    inquirer.prompt.withArgs(questions).resolves({[commonLanguagePrompts.questionNames.UNIT_TESTS]: false});
+    inquirer.prompt
+      .withArgs(questions)
+      .resolves({
+        [commonLanguagePrompts.questionNames.UNIT_TESTS]: false,
+        [commonLanguagePrompts.questionNames.CI_SERVICE]: chosenService
+      });
+    ciScaffolder.default
+      .withArgs(ciServices, chosenService, {projectRoot})
+      .resolves({...any.simpleObject(), vcsIgnore: {files: ciFilesToIgnore, directories: ciDirectoriesToIgnore}});
 
     const result = await scaffold({projectRoot, projectName, description, vcs, ciServices, visibility});
 
@@ -58,7 +71,7 @@ $ make test
         projectDetails: {},
         badges: {consumer: {}, status: {}, contribution: {}},
         verificationCommand: 'make test',
-        vcsIgnore: {files: [], directories: ['/deps']}
+        vcsIgnore: {files: ciFilesToIgnore, directories: ['/deps', ...ciDirectoriesToIgnore]}
       }
     );
     assert.calledWith(
@@ -87,6 +100,7 @@ clean:
 
   test('that testing tool installation instructions are included when the project will be unit tested', async () => {
     inquirer.prompt.withArgs(questions).resolves({[commonLanguagePrompts.questionNames.UNIT_TESTS]: true});
+    ciScaffolder.default.resolves({vcsIgnore: {files: [], directories: []}});
 
     const result = await scaffold({projectRoot, projectName, description, vcs, ciServices, visibility});
 
